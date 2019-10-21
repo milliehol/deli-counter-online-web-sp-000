357158aab3d17ee9677a4893c1563256590823f9
Carla Holcomb <carla.programmer@gmail.com>
2019-10-21 17:47:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,37 @@
 # Write your code here.
+
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+
+  else
+
+
+narray = []
+sarray = nil
+katz_deli.each_with_index do |name, index|
+  narray << " #{index+1}. #{name}"
+
+end
+sarray = narray.join
+puts "The line is currently:#{sarray}"
+
+
+end
+end
+
+def take_a_number(katz_deli, name)
+nindex = 0
+  katz_deli.each_with_index do |name2, index|
+if name == name2
+    nindex = index + 1
+else
+  nindex = nil
+end
+  end
+
+puts "Welcome #{name}.  You are currently #{nindex} in line."
+
+
+end
